6270aa4c614d7f007747946e056079d1dba381e7
DolVolo <donny21001@gmail.com>
2025-09-07 23:12:49 +0700

    nav click object

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,16 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class PlayerMovement : MonoBehaviour{
+
+    Camera cam;
+    public LayerMask groundMask;
+    public PlayerAnimation playerAnimation;
+    
+    void Start(){
+        cam = Camera.main;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        // Using new Input System
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Debug.Log("Mouse clicked!"); // Test if this appears first
+            
+            Vector2 mousePosition = Mouse.current.position.ReadValue();
+            Ray ray = cam.ScreenPointToRay(mousePosition);
+            RaycastHit hit;
+            
+            // Try raycast without layer mask first
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Hit object: " + hit.collider.name);
+
+            }
+            else
+            {
+                Debug.Log("No object hit by raycast");
+            }
+        }
     }
 }
+
+    
+
